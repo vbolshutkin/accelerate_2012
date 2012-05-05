bb26bea7dbea25b88f489bf31a1b6d582cd849d9
Vladimir Bolshutkin <enargit@gmail.com>
2012-05-05 09:10:30 +0200

    Offset algorithm

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@
 #include <pthread.h>
 #include <omp.h>
 
-
 #include <smmintrin.h> // SSE 4.2
 
 #include "common.h"
@@ -48,6 +47,7 @@ typedef struct {
 
 uint minMatchLength;
 uint m;
+uint m4;
 
 uint n_threads;
 
@@ -89,24 +89,24 @@ char decode_char(char c) {
 }
 
 void print_seq(seq* s) {
-	printf("seq %s (len=%d):", s->name, s->len);
-	int i;
-	for (i = 0; i < s->len; ++i) {
+        printf("seq %s (len=%d):", s->name, s->len);
+        int i;
+        for (i = 0; i < s->len; ++i) {
 
-		unsigned char c = s->ptr[i];
+                unsigned char c = s->ptr[i];
 
-		c = decode_char(c % 4);
+                c = decode_char(c % 4);
 
-		printf("%c", c);
-	}
-	printf("\n");
+                printf("%c", c);
+        }
+        printf("\n");
 }
 
 void put_char(char c, unsigned char* ptr, uint index) {
 
     c = encode_char(c);
 
-	uint step = m;
+	uint step = m4;
 
 	char ix;
 	for (ix = 1; (ix < 4) && (index >= step * ix); ++ix) {
@@ -250,204 +250,17 @@ void read_input_file(char* fname) {
 
 
 
-typedef enum { ANY, LEFT, RIGHT } Direction;
 
-void check_sa(int * ref_suffix_array, uint len, uint ref_offset) {
-	int i; long s1 = 0, s2 = 0;
-	for (i= 0; i < len; ++i) {
-		s1 += ref_suffix_array[i];
-		s2 += i;
-	}
-	if (s1 == s2 ) {
-		printf("sa %ld of length %d is ok\n", ref_suffix_array, len);
-	} else {
-		printf("WTF? %ld %ld\n", s1, s2);
-	}
-}
-
-int dihotomy(int* suffix_array,
-		 int n_offset,
-		 int lo, int hi,
-		 const unsigned char* haystack,
-	     const unsigned char* needle, Direction dir) {
-
-	unsigned char nc = needle[n_offset];
-
-	while (hi >= lo) {
-
-		// check hi and lo
-		if (dir == LEFT || dir == ANY) {
-			if (haystack[suffix_array[lo] + n_offset] == nc) {
-				return lo;
-			}
-		}
-		if (dir == RIGHT || dir == ANY) {
-			if (haystack[suffix_array[hi] + n_offset] == nc) {
-				return hi;
-			}
-		}
-		if (dir == LEFT) {
-			if (haystack[suffix_array[hi-1] + n_offset] != nc) {
-				return hi;
-			}
-		}
-		if (dir == RIGHT) {
-			if (haystack[suffix_array[lo+1] + n_offset] != nc) {
-				return lo;
-			}
-		}
-
-
-		//printf("hi lo %d %d\n", hi, lo);
-		uint mid = (hi + lo) / 2;
-		uint h_index = suffix_array[mid];
-		unsigned char hc = haystack[h_index + n_offset];
-
-		//printf("middle\n");
-
-		if (hc > nc) {
-			if (dir != ANY) {
-				if (hi == mid) {
-					return mid; // FIXME why not break?
-				}
-				hi = mid;
-			} else {
-				hi = mid - 1;
-			}
-		} else if (hc < nc) {
-			if (dir != ANY) {
-				if (lo == mid) {
-					return mid; // FIXME why not break?
-				}
-				lo = mid;
-			} else {
-				lo = mid + 1;
-			}
-		} else {
-			//printf("eq before\n");
-			if (dir == LEFT) {
-				if (haystack[suffix_array[mid-1] + n_offset] != nc) {
-					return mid;
-				}
-				hi = mid;
-			} else if (dir == RIGHT) {
-				if (haystack[suffix_array[mid+1] + n_offset] != nc) {
-					return mid;
-				}
-				lo = mid;
-			} else { // dir == ANY
-				return mid;
-			}
-
-		}
-	}
-    return -1;
-}
-
-void substring_found(const unsigned char* substr, const unsigned char* haystack, uint hlimit,
-	     const unsigned char* needle, uint noffset, uint subtask_ref_offset) {
-	uint hs_offset = substr - haystack;
-
-	if (subtask_ref_offset + hs_offset > 0 && noffset > 0) {
-		if (substr[-1] == needle[-1]) {
-			return;
-		}
-	}
-
-	uint commonlen = m * 4;
-
-	uint i;
-	for (i = m; i < hlimit - hs_offset - m * 3; ++i) {
-		if (haystack[hs_offset + i] != needle[i]) {
-			break;
-		}
-		++commonlen;
-	}
-
-	if (commonlen < minMatchLength) {
-		return;
-	}
-
-	ans a;
-	ans *out = &a;
-
-	out->othoffset = noffset + 1;
-	out->othlimit = out->othoffset + commonlen - 1;
-	out->refoffset = subtask_ref_offset + hs_offset + 1;
-	out->reflimit = out->refoffset + commonlen - 1;
-
-	// debug output
-	printf("%d %d %d %d\n", a.refoffset, a.reflimit, a.othoffset, a.othlimit);
-}
-
-void substring_suffix_array(int* suffix_array,
-		 const unsigned char* haystack, size_t hlen,
-	     const unsigned char* needle,   size_t nlen,
-	     uint noffset, uint subtask_ref_offset) {
-
-	int lo = 0, hi = hlen - 1;
-
-	int n_offset = 0;
-
-	int lb = lo, rb = hi;
-
-	while (n_offset < nlen) {
-		int d = dihotomy(suffix_array, n_offset, lo, hi, haystack, needle, ANY);
-		if (d == -1) {
-			return;
-		}
-		lb = dihotomy(suffix_array, n_offset, lo, d, haystack, needle, LEFT);
-		rb = dihotomy(suffix_array, n_offset, d, hi, haystack, needle, RIGHT);
-		if (lb == rb && lb != -1) {
-			char eq = suffix_array[lb] >= hlen - nlen ||
-					memcmp(haystack + suffix_array[lb] + n_offset, needle + n_offset, nlen - n_offset);
-
-			if (eq == 0) {
-				break;
-			}
-		}
-
-		lo = lb;
-		hi = rb;
-		++n_offset;
-	}
-
-	// here can be a lot of matches
-	int i;
-	for (i = lb; i <= rb; ++i) {
-		substring_found(haystack + suffix_array[i], haystack, hlen, needle, noffset, subtask_ref_offset);
-	}
-}
-
-void solve_task(task_t * st) {
-
-	int * MALLOC_IT(ref_suffix_array, st->ref->len);
-	suffixArray(st->ref->ptr, ref_suffix_array, st->ref->len, 256);
-
-	int i;
-	for (i = 0; i < st->n_otherseqs; ++i) {
-		seq oth = st->otherseqs[i];
-		uint oth_iter;
-		for (oth_iter = 0; oth_iter < oth.len - minMatchLength;
-				++oth_iter) {
-
-			substring_suffix_array(ref_suffix_array, st->ref->ptr,
-					st->ref->len, oth.ptr + oth_iter, m,
-					oth_iter, st->ref_offset);
-		}
-	}
-
-	free(ref_suffix_array);
-}
 
 void decompose_task(task_t * parent, char parts, task_t * out) {
 
 	int plen = parent->ref->len;
+	int len = plen / parts + m4;
 
 	int i;
 	for (i = 0; i < parts; ++i) {
 		int offset = i * plen / parts;
-		int len = plen / parts + m / 4;
+
 		if (len >= plen - offset) {
 			len = plen - offset;
 		}
@@ -461,13 +274,6 @@ void decompose_task(task_t * parent, char parts, task_t * out) {
 
 }
 
-void *thread_func_solve(void *vptr_args)
-{
-	task_t *task = (task_t*)vptr_args;
-	solve_task(task);
-    return NULL;
-}
-
 int main(int argc, char* argv[]) {
 
 	DECLARE_TIME_MEASUREMENT;
@@ -479,7 +285,8 @@ int main(int argc, char* argv[]) {
 
 
 	minMatchLength = atol(argv[2]);
-	m = minMatchLength / 4;
+	m4 = minMatchLength / 4;
+	m = m4  + minMatchLength % 4;
 
 	read_ref_file(argv[3]);
 	read_input_file(argv[4]);
@@ -488,21 +295,52 @@ int main(int argc, char* argv[]) {
 	printf("reading took %ld ms\n", TIME_ELAPSED);
 	START_TIME_MEASURE;
 
-    task_t task = {&refseq, n_otherseqs, otherseqs, 0};
 
-    char parts = n_threads;
+	seq oth = otherseqs[0];
+
+    long offset;
+    for (offset = (int) (m - oth.len + 1); offset <= refseq.len - m; ++offset) {
+    	uint ref_offset;
+    	uint oth_offset;
+    	if (offset < 0) {
+    		ref_offset = 0;
+    		oth_offset = - offset;
+    	} else {
+    		ref_offset = offset;
+    		oth_offset = 0;
+    	}
+    	unsigned char *ref_ptr = refseq.ptr + ref_offset;
+    	unsigned char *oth_ptr = oth.ptr + oth_offset;
+
+    	uint iter = 0;
+
+    	int cnt = 0;
+    	while (iter < refseq.len - ref_offset && iter < oth.len - oth_offset) {
+    		if (ref_ptr[iter] == oth_ptr[iter]) {
+    			++cnt;
+    		} else {
+    			if (cnt >= m) {
+    				printf("%d %d %d %d\n", ref_offset + iter - cnt + 1, ref_offset + iter + 3 * m4,  oth_offset + iter - cnt + 1, oth_offset + iter + 3 * m4);
+    			}
+    			cnt = 0;
+
+//    			// fast forward works slowly
+//    			uint back_iter = iter + m;
+//    			while (ref_ptr[back_iter] == oth_ptr[back_iter]) {
+//    				--back_iter;
+//    				++cnt;
+//    			}
+
+    		}
+    		++iter;
+    	}
+    	if (cnt >= m) {
+			printf("%d %d %d %d\n", ref_offset + iter - cnt + 1, ref_offset + iter,  oth_offset + iter - cnt + 1, oth_offset + iter);
+		}
+    }
 
 
-    task_t * MALLOC_IT(subtasks, parts);
-    decompose_task(&task, parts, subtasks);
-    int i;
 
-	#pragma omp parallel for private(i)
-	for (i = 0; i < parts; ++i) {
-		solve_task(&subtasks[i]);
-	}
-
-    free(subtasks);
 
 
 	FINISH_TIME_MEASURE;
